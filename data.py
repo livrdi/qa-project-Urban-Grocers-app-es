ee73248b2f94bb266d013045c9174ae8e9a2ed08
livrdi <vikydelvi@gmail.com>
2024-11-06 23:22:51 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,35 @@
-"}
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Juan",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+}
+
+
+# Lista de comprobación de pruebas
+
+kit_body_test_1 = "A",
+
+kit_body_test_2 = "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC"
+
+kit_body_test_3 = ""
+
+kit_body_test_4= "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD"
+
+kit_body_test_5= "\"№%@\","
+
+kit_body_test_6 = "A Aaa"
+
+kit_body_test_7 = "123"
+
+kit_body_test_8 = { }
+
+kit_body_test_9 = 123
+
+
+kit_body = {
+    "name": "a"
+}
