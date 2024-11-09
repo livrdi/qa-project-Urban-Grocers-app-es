866333b884dbf57f6734b16d67bc2e63187e3310
livrdi <vikydelvi@gmail.com>
2024-11-08 22:31:34 -0600

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,39 +1,40 @@
-from data import kit_body
 import configuration
 import requests
 import data
+import json
+
+
+def get_docs():
+  return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+
+
+def get_logs():
+  return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH)
 
 
 def get_users_table():
-    return requests.get(configuration.URL_SERVICE + configuration.CREATE_USER_PATH)
-
-response = get_users_table()
-print(response.status_code)
-
-def post_new_client_kit(body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # coloca URL completa
-                         json=body,  # inserta el cuerpo de solicitud
-                         headers=data.headers)  # inserta los encabezados
-
-response = post_new_client_kit(data.user_body)
-print(response.status_code)
-print(response.json())
+  return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
 
 
+def post_new_user(body):
+  return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                       json=body,  # inserta el cuerpo de solicitud
+                       headers=data.headers)  # inserta los encabezados
 
-def get_kit_body():
-    return requests.get(configuration.URL_SERVICE + configuration.CREATE_KITS_PATH,
-                         json=kit_body,
-                         headers=data.headers)
-response = get_kit_body()
-print(response.status_code)
-print(response.json())
 
-def post_new_client_kit(kit_body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_KITS_PATH,
-                         json=kit_body, # Datos que envía la solicitud
-                         headers=data.headers)
+def post_products_kits(products_ids):
+  return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                       json=products_ids,
+                       headers=data.headers)
 
-response = post_new_client_kit(data.kit_body)
-print(response.status_code)
-print(response.json())
+
+def post_new_kit(body, auth_token):
+  headers = data.headers
+  headers["Authorization"] = f"Bearer {auth_token}"
+
+  payload = json.dumps(body)
+
+  return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
+                       headers=headers,
+                       data=payload
+                       )
