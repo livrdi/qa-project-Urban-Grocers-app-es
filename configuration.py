7e08abfe81a0f054e661071b164e0723079cbde0
livrdi <vikydelvi@gmail.com>
2024-11-21 22:01:09 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
 URL_SERVICE = "https://cnt-e7f141ef-9571-4133-befc-d50b93e0b107.containerhub.tripleten-services.com" #Dirección de URL
 CREATE_USER_PATH = "/api/v1/users"# ruta para crear un usuario
 CREATE_KITS_PATH = "api/v1/kits" # ruta para crear un kit
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
